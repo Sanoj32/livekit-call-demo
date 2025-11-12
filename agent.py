ca302623c6666ebf7dda033b3e09048c296ca405
Sanoj Raj Shrestha <sanoj.shrestha.13@gmail.com>
2025-11-12 10:43:37 +0545

    agent call with python

---
+++ agent.py
@@ -0,0 +1,57 @@
+import os
+
+from dotenv import load_dotenv
+
+from livekit.plugins import openai
+from livekit import agents
+from livekit.agents import AgentSession, Agent, RoomInputOptions
+from livekit.plugins import noise_cancellation, silero
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+load_dotenv(".env.local")
+
+
+class Assistant(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="""You are a helpful voice AI assistant.
+            You eagerly assist users with their questions by providing information from your extensive knowledge.
+            Your responses are concise, to the point, and without any complex formatting or punctuation including emojis, asterisks, or other symbols.
+            You are curious, friendly, and have a sense of humor.""",
+        )
+
+
+async def entrypoint(ctx: agents.JobContext):
+    session = AgentSession(
+        stt = openai.STT.with_azure(
+            model="gpt-4o-transcribe",
+        ),
+        llm=openai.LLM.with_azure(
+            azure_deployment="gpt-5-mini",
+            azure_endpoint=os.getenv("AZURE_OPENAI_ENDPOINT"), # or AZURE_OPENAI_ENDPOINT
+            api_key=os.getenv("AZURE_OPENAI_API_KEY"), # or AZURE_OPENAI_API_KEY
+            api_version="2024-10-01-preview", # or OPENAI_API_VERSION
+        ),
+        tts=openai.TTS.with_azure(
+            model="gpt-4o-mini-tts",
+            voice="coral",
+        ),
+        vad=silero.VAD.load(),
+    )
+
+    await session.start(
+        room=ctx.room,
+        agent=Assistant(),
+        room_input_options=RoomInputOptions(
+            # For telephony applications, use `BVCTelephony` instead for best results
+            noise_cancellation=noise_cancellation.BVC(),
+        ),
+    )
+
+    await session.generate_reply(
+        instructions="Greet the user and offer your assistance."
+    )
+
+
+if __name__ == "__main__":
+    agents.cli.run_app(agents.WorkerOptions(entrypoint_fnc=entrypoint))
